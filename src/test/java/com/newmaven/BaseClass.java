47d1671edab654c0a5c9e8eac15b2d1a87022e35
GigiMann27 <gmann0827@gmail.com>
2018-12-29 12:30:36 -0500

    new

--- src/test/java/com/newmaven/BaseClass.java
+++ src/test/java/com/newmaven/BaseClass.java
@@ -3,15 +3,16 @@ package com.newmaven;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+
 
 public class BaseClass {
+	
 	@Test
 	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver","/Users/Gurpreet/eclipse-workspace/com.maventestNgJen/Drivers/chromedriver");
 		
 		WebDriver driver= new ChromeDriver();
-		driver.get("http://www.google.com");	
+		driver.get("https://www.google.com");	
 	}
 	
 }
